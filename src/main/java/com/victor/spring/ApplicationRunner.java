e8fad7f418ae2db5002885a9c42036cee8587c82
Viktor <vitya.malahov.95@gmail.com>
2023-04-02 10:39:24 +0300

    lesson 5

--- src/main/java/com/victor/spring/ApplicationRunner.java
+++ src/main/java/com/victor/spring/ApplicationRunner.java
@@ -7,12 +7,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 
-        ConnectionPool pool = context.getBean("p1", ConnectionPool.class);
-        System.out.println(pool);
+            ConnectionPool pool = context.getBean("p1", ConnectionPool.class);
+            System.out.println(pool);
 
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(companyRepository);
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println(companyRepository);
+        }
     }
 }
