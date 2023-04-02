d057d62adbfaafe52770ccc696c8485e1c406efb
Viktor <vitya.malahov.95@gmail.com>
2023-04-02 15:44:55 +0300

    lesson 8 create custom BPP and override postProcessBeforeInitialization

--- src/main/java/com/victor/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/victor/spring/database/repository/CompanyRepository.java
@@ -1,16 +1,10 @@
 package com.victor.spring.database.repository;
 
+import com.victor.spring.database.bpp.InjectBean;
 import com.victor.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
-
-    private CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool){
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
