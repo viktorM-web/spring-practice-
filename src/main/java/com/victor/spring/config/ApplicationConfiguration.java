5df0607d596bb1774c9c599b9aa561e6980bb23d
Viktor <vitya.malahov.95@gmail.com>
2023-04-03 10:53:33 +0300

    lesson 15 @Import @ImportResource

--- src/main/java/com/victor/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/victor/spring/config/ApplicationConfiguration.java
@@ -1,14 +1,19 @@
 package com.victor.spring.config;
 
 import com.victor.spring.database.repository.CrudRepository;
+import com.victor.web.WebConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.context.annotation.ComponentScan.*;
 
+//@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.victor.spring",
---
+++ src/main/java/com/victor/web/WebConfiguration.java
@@ -0,0 +1,7 @@
+package com.victor.web;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+}
