fde61ebadb645a324e33c00af8f2e4600663d8e0
Viktor <vitya.malahov.95@gmail.com>
2023-04-05 00:35:42 +0300

    lesson 20 spring boot starter

--- src/main/java/com/victor/spring/ApplicationRunner.java
+++ src/main/java/com/victor/spring/ApplicationRunner.java
@@ -1,25 +1,12 @@
 package com.victor.spring;
 
-import com.victor.spring.config.ApplicationConfiguration;
-import com.victor.spring.database.pool.ConnectionPool;
-import com.victor.spring.database.repository.CrudRepository;
-import com.victor.spring.service.CompanyService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web", "prod");
-            context.refresh();
-
-            ConnectionPool pool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(pool);
-
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
