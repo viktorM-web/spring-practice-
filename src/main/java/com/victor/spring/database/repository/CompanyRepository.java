86a2d03e3f663d6bfe9275112f1e4b83d490767f
Viktor <vitya.malahov.95@gmail.com>
2023-04-05 10:39:41 +0300

    lesson 21 add annotation over CompanyRepository

--- src/main/java/com/victor/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/victor/spring/database/repository/CompanyRepository.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
+@Repository
 @Scope(SCOPE_PROTOTYPE)
 @Transaction
 @Auditing
