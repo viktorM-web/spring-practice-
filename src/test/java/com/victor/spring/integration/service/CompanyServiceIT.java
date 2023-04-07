76c2ff75df7c360c308f6e1e85fa23d0311d856a
Viktor <vitya.malahov.95@gmail.com>
2023-04-07 12:00:55 +0300

    lesson 30 Integration Test

--- src/test/java/com/victor/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/victor/spring/integration/service/CompanyServiceIT.java
@@ -1,21 +1,28 @@
 package com.victor.spring.integration.service;
 
+import com.victor.spring.config.DatabaseProperties;
 import com.victor.spring.dto.CompanyReadDto;
+import com.victor.spring.integration.annotation.IT;
 import com.victor.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
+//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    private CompanyService companyService;
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
