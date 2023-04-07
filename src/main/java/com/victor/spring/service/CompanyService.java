b43ccea2015a6e7900b65ea4d2c5253d18179c3d
Viktor <vitya.malahov.95@gmail.com>
2023-04-08 02:27:04 +0300

    lesson 33 @Transactional

--- src/main/java/com/victor/spring/service/CompanyService.java
+++ src/main/java/com/victor/spring/service/CompanyService.java
@@ -7,6 +7,7 @@ import com.victor.spring.listner.entity.EntityEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -20,6 +21,7 @@ public class CompanyService {
     private final UserService userService;
     private final ApplicationEventPublisher eventPublisher;
 
+    @Transactional
     public Optional<CompanyReadDto> findById(Integer id){
         return companyRepository.findById(id)
                 .map(entity ->{
