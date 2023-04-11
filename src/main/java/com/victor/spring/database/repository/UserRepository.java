6e361d76ab7d6604a67827c5ae35a7b5c83fd0e8
Viktor <vitya.malahov.95@gmail.com>
2023-04-11 23:50:43 +0300

    lesson 46 hibernate envers

--- src/main/java/com/victor/spring/database/repository/UserRepository.java
+++ src/main/java/com/victor/spring/database/repository/UserRepository.java
@@ -11,12 +11,16 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.history.RevisionRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long>, FilterUserRepository {
+public interface UserRepository extends
+        JpaRepository<User, Long>,
+        FilterUserRepository,
+        RevisionRepository<User, Long, Integer> {
 
     @Query("select u from User u " +
            "where u.firstname like %:firstname% and u.lastname like %:lastname%")
