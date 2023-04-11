9ff532e2786d4b8e8cedb05b0877b006759191d9
Viktor <vitya.malahov.95@gmail.com>
2023-04-11 12:44:32 +0300

    lesson 39 @Query with @Modifying

--- src/main/java/com/victor/spring/database/repository/UserRepository.java
+++ src/main/java/com/victor/spring/database/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.victor.spring.database.repository;
 
+import com.victor.spring.database.entity.Role;
 import com.victor.spring.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -15,4 +17,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(nativeQuery = true,
             value = "SELECT u.* FROM users u WHERE u.username = :username")
     List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update User u " +
+           "set u.role = :role " +
+           "where  u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
 }
--- src/test/java/com/victor/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/victor/spring/integration/database/repository/UserRepositoryTest.java
@@ -1,20 +1,40 @@
 package com.victor.spring.integration.database.repository;
 
+import com.victor.spring.database.entity.Role;
 import com.victor.spring.database.entity.User;
 import com.victor.spring.database.repository.UserRepository;
 import com.victor.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 @IT
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
     private final UserRepository userRepository;
+
+    @Test
+    void checkUpdate(){
+        User ivan = userRepository.getById(1L);
+        assertSame(Role.ADMIN, ivan.getRole());
+        ivan.setBirthDate(LocalDate.now());
+
+        int resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, resultCount);
+
+//        ivan.getCompany().getName(); //LazyInitialisation exception; Ivan detached entity
+
+        User theSameIvan = userRepository.getById(1L);
+        assertSame(Role.USER, theSameIvan.getRole());
+    }
 
     @Test
     void checkQueries() {
