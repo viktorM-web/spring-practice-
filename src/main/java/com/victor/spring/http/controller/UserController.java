970d795a43fa05b2af58c6ea33469a28bd9e9c68
Viktor <vitya.malahov.95@gmail.com>
2023-04-16 23:14:58 +0300

    lesson 58 Spring MVC Testing

--- src/main/java/com/victor/spring/http/controller/UserController.java
+++ src/main/java/com/victor/spring/http/controller/UserController.java
@@ -6,11 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
@@ -40,7 +38,7 @@ public class UserController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+//    @ResponseStatus(HttpStatus.CREATED)
     public String create(UserCreateEditDto user) {
         return "redirect:/users/" + userService.create(user).getId();
     }
