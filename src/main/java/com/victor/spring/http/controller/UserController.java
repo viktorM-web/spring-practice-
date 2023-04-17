53d8826858b21d4afab31453229d3be253168549
Viktor <vitya.malahov.95@gmail.com>
2023-04-17 11:15:10 +0300

    lesson 61 Thymeleaf CRUD ViewLayer

--- src/main/java/com/victor/spring/http/controller/UserController.java
+++ src/main/java/com/victor/spring/http/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.victor.spring.http.controller;
 
+import com.victor.spring.database.entity.Role;
 import com.victor.spring.dto.UserCreateEditDto;
+import com.victor.spring.service.CompanyService;
 import com.victor.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
@@ -19,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserController {
 
     private final UserService userService;
+    private final CompanyService companyService;
 
     @GetMapping
     public String findAll(Model model) {
@@ -31,6 +33,8 @@ public class UserController {
         return userService.findById(id)
                 .map(user -> {
                     model.addAttribute("user", user);
+                    model.addAttribute("roles", Role.values());
+                    model.addAttribute("companies", companyService.findAll());
                     return "user/user";
                 })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
