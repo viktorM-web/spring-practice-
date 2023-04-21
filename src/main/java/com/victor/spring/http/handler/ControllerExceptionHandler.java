6d2a67f2dc3d908eb1f21fcde1a3a1bb32e6b8b9
Viktor <vitya.malahov.95@gmail.com>
2023-04-21 10:07:07 +0300

    lesson 68 REST

--- src/main/java/com/victor/spring/http/handler/ControllerExceptionHandler.java
+++ src/main/java/com/victor/spring/http/handler/ControllerExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
-@ControllerAdvice
+@ControllerAdvice(basePackages = "com.victor.spring.http.controller")
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
