b757cea39ba71cce40e19b4b5e1d0d450d4db260
Viktor <vitya.malahov.95@gmail.com>
2023-05-01 01:01:23 +0300

    lesson 73 Form Login

--- src/main/java/com/victor/spring/http/controller/LoginController.java
+++ src/main/java/com/victor/spring/http/controller/LoginController.java
@@ -1,10 +1,7 @@
 package com.victor.spring.http.controller;
 
-import com.victor.spring.dto.LoginDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -14,8 +11,4 @@ public class LoginController {
         return "user/login";
     }
 
-    @PostMapping("/login")
-    public String login(Model model, LoginDto loginDto){
-        return "redirect:https://google.com";
-    }
 }
